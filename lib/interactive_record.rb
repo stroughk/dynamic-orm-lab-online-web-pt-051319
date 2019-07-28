49331c6c006ced9c5aa3be8862ff5e5304f75b6a
Kateryna Strough <katerynastrough@hotmail.com>
2019-07-28 23:38:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,12 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  def self.table_name
+     "#{self.to_s.downcase}s"
+   end
+
+
+
   
 end
